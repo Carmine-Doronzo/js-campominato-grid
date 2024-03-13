ab80b26f9823327a51bd4a7889a836a914509bc5
Carmine-Doronzo <carmine.doronzo92@gmail.com>
2024-03-13 15:29:26 +0100

    ADD ';'

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
-console.log('Campo minato')
+console.log('Campo minato');
 
 const playDomElement = document.getElementById('play');
 
 const containerDomElement = document.querySelector('.container');
 
-const resetDomElement = document.getElementById('reset')
+const resetDomElement = document.getElementById('reset');
 
 resetDomElement.addEventListener('click',function(){
     containerDomElement.innerHTML = "";
@@ -18,20 +18,20 @@ playDomElement.addEventListener('click', function(){
     let gridElement ;
 
     if(dificultyDomElement == '1'){
-        console.log('difficile')
-        containerDomElement.classList.add('dif1')
+        console.log('difficile');
+        containerDomElement.classList.add('dif1');
         cellElement = 10;
         gridElement = cellElement ** 2;
         //console.log(gridElement)
     }else if(dificultyDomElement == '2'){
-        console.log('medio')
-        containerDomElement.classList.add('dif2')
+        console.log('medio');
+        containerDomElement.classList.add('dif2');
         cellElement = 9;
         gridElement = cellElement ** 2;
        // console.log(gridElement)
     }else{
-        console.log('facile')
-        containerDomElement.classList.add('dif3')
+        console.log('facile');
+        containerDomElement.classList.add('dif3');
         cellElement = 7;
         gridElement = cellElement ** 2;
         //console.log(gridElement)
@@ -42,13 +42,13 @@ playDomElement.addEventListener('click', function(){
     for(let i = 0; i < gridElement; i++){
        let numCell = i + 1;
         //console.log(i)
-        let cellDomElement = document.createElement('div')
-        cellDomElement.classList.add('cell')
-        cellDomElement.innerHTML =`${numCell}`
+        let cellDomElement = document.createElement('div');
+        cellDomElement.classList.add('cell');
+        cellDomElement.innerHTML =`${numCell}`;
         containerDomElement.append(cellDomElement);
         cellDomElement.addEventListener('click', function(){
-            cellDomElement.classList.toggle('bg-azure')
-            console.log('click sulla casella :', numCell)
+            cellDomElement.classList.toggle('bg-azure');
+            console.log('click sulla casella :', numCell);
         })
     }
 
