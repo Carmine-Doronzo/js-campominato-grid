03aab4feea01eb94b973957e2355df378327f92f
Carmine-Doronzo <carmine.doronzo92@gmail.com>
2024-03-13 15:28:12 +0100

    ADD reset button

--- js/script.js
+++ js/script.js
@@ -4,6 +4,12 @@ const playDomElement = document.getElementById('play');
 
 const containerDomElement = document.querySelector('.container');
 
+const resetDomElement = document.getElementById('reset')
+
+resetDomElement.addEventListener('click',function(){
+    containerDomElement.innerHTML = "";
+})
+
 playDomElement.addEventListener('click', function(){
 
     let dificultyDomElement = document.getElementById('dificulty').value;
@@ -13,16 +19,19 @@ playDomElement.addEventListener('click', function(){
 
     if(dificultyDomElement == '1'){
         console.log('difficile')
+        containerDomElement.classList.add('dif1')
         cellElement = 10;
         gridElement = cellElement ** 2;
         //console.log(gridElement)
     }else if(dificultyDomElement == '2'){
         console.log('medio')
+        containerDomElement.classList.add('dif2')
         cellElement = 9;
         gridElement = cellElement ** 2;
        // console.log(gridElement)
     }else{
         console.log('facile')
+        containerDomElement.classList.add('dif3')
         cellElement = 7;
         gridElement = cellElement ** 2;
         //console.log(gridElement)
@@ -38,9 +47,12 @@ playDomElement.addEventListener('click', function(){
         cellDomElement.innerHTML =`${numCell}`
         containerDomElement.append(cellDomElement);
         cellDomElement.addEventListener('click', function(){
-            cellDomElement.classList.add('bg-azure')
+            cellDomElement.classList.toggle('bg-azure')
             console.log('click sulla casella :', numCell)
         })
     }
 
+    
+
 })
+
